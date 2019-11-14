2ae89e5b9e2a8d069b06622befa073f22fd97dff
Raudel Flores <rawdell@outlook.com>
2019-11-13 22:00:34 -0600

    created a timer, still working on getting it to count down instead of up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import { timeout } from "q";
+
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   let [home, setHome] = useState(0);
   let [away, setAway] = useState(0);
+  
+  // Timer
+  let [time, setTime] = useState(0);
+  let secondsPassed = useRef(0);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTime(time + 1);
+    }, 1000);
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [time]);
 
   // Touchdowns
   const touchHome = point => {
@@ -24,22 +38,27 @@ function App() {
     setAway(away + 3);
   };
 
+let scoreboardColor = '#003E7A';
+let gold = '#FFC61E';
+let red = 'red';
+let white = 'white';
+let silver = 'silver';
 
   return (
     <div className="container">
-      <section className="scoreboard">
+      <section className="scoreboard" style={{backgroundColor: scoreboardColor }}>
         <div className="topRow">
-          <div className="home">
+          <div className="home" style={{backgroundColor: gold }}>
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{home}</div>
+            <div className="home__score" style={{color: white }}>{home}</div>
           </div>
-          <div className="timer">00:03</div>
-          <div className="away">
+          <div className="timer" style={{color: red }}>{time}</div>
+          <div className="away" style={{backgroundColor: silver }}>
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{away}</div>
+            <div className="away__score" style={{color: white }}>{away}</div>
           </div>
         </div>
         <BottomRow />
